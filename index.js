e7ce1d0c452460e68b54058369a8ea2b3475521b
ri-ca4 <riley.de12@gmail.com>
2022-05-25 18:38:50 -0400

    attempt to fix jquery on heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -43,4 +43,4 @@ app.post("/story3", (req, res) => {
     })
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 5000);
